e7444ec05bae59f41abd37d2b5eea4662b01f5ae
Chamodya Ganegamage <139947960+chamo7788@users.noreply.github.com>
2025-01-03 11:03:08 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+// Importing the defineConfig helper from Vite
 import { defineConfig } from 'vite'
+
+// Importing the React plugin for Vite
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Adding the React plugin to the Vite configuration
   plugins: [react()],
 })
